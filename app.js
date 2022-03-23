85ada9ac3ed2d306ba32453f61839861e5c8b737
mremoto <mremotoo@gmail.com>
2022-03-23 15:03:25 +0000

    Position divs, add style

---
+++ app.js
@@ -0,0 +1,77 @@
+const container_div = document.querySelector(".container_div")
+
+for (let i = 0; i < 256; i++) {
+
+    const child_div = document.createElement("div")
+    child_div.classList.add("child_div")
+    container_div.appendChild(child_div)
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// container_div.appendChild(child_div)
+
+// const child_div1 = document.createElement("div")
+// const child_div2 = document.createElement("div")
+// const child_div3 = document.createElement("div")
+// const child_div4 = document.createElement("div")
+// const child_div5 = document.createElement("div")
+// const child_div6 = document.createElement("div")
+// const child_div7 = document.createElement("div")
+// const child_div8 = document.createElement("div")
+// const child_div9 = document.createElement("div")
+// const child_div10 = document.createElement("div")
+// const child_div11 = document.createElement("div")
+// const child_div12 = document.createElement("div")
+// const child_div13 = document.createElement("div")
+// const child_div14 = document.createElement("div")
+// const child_div15 = document.createElement("div")
+// const child_div16 = document.createElement("div")
+// const child_div17 = document.createElement("div")
+// const child_div18 = document.createElement("div")
+// const child_div19 = document.createElement("div")
+// const child_div20 = document.createElement("div")
+// const child_div21 = document.createElement("div")
+// const child_div22 = document.createElement("div")
+// const child_div23 = document.createElement("div")
+// const child_div24 = document.createElement("div")
+// const child_div25 = document.createElement("div")
+// const child_div26 = document.createElement("div")
+// const child_div27 = document.createElement("div")
+// const child_div28 = document.createElement("div")
+// const child_div29 = document.createElement("div")
+// const child_div30 = document.createElement("div")
+// const child_div31 = document.createElement("div")
+// const child_div32 = document.createElement("div")
