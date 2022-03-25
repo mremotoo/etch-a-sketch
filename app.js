b23997ac2445252ec17a2be333d53445338e66ac
mremoto <mremotoo@gmail.com>
2022-03-25 01:30:34 +0000

    click and hold function change from hover

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const container_div = document.querySelector(".container_div")
+let holding = false
+const clearBtn = document.querySelector(".clear").addEventListener("click", resetBoard)
 
 for (let i = 0; i < 256; i++) {
 
@@ -7,19 +9,25 @@ for (let i = 0; i < 256; i++) {
     container_div.appendChild(child_div)
 }
 
-function mouseHover(e) {
-    
-    this.classList.add("childDivHovered")
-}
-
 function resetBoard(){
-    console.log("reset")
     childDivs.forEach(child_div => child_div.classList.remove("childDivHovered"))
 }
 
 
-
+//Click hold and drag event
 const childDivs = document.querySelectorAll('.child_div')
-childDivs.forEach(child_div => child_div.addEventListener('mouseover', mouseHover))
 
-const clearBtn = document.querySelector(".clear").addEventListener("click", resetBoard)
+childDivs.forEach(child_div => child_div.addEventListener('mousedown', function(e) {
+    holding = true
+    this.classList.add("childDivHovered")
+}))
+childDivs.forEach(child_div => child_div.addEventListener('mouseup', function(e) {
+    holding = false   
+}))
+childDivs.forEach(child_div => child_div.addEventListener('mousemove', function(e) {
+    if (holding) {
+        this.classList.add("childDivHovered")
+    }    
+}))
+
+
